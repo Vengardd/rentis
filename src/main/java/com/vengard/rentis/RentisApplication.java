227dcc7637d45413b1bd25181f233d34f1c5ac44
Vengardd <vengardd@users.noreply.github.com>
2018-08-18 11:15:24 +0200

    initial commit

---
+++ src/main/java/com/vengard/rentis/RentisApplication.java
@@ -0,0 +1,12 @@
+package com.vengard.rentis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vengard/rentis/RentisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vengard.rentis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RentisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
